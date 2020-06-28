bdf82d51df73636384c9a043e6453e7c9d8ccbe4
juandigv69 <juandigv69@gmail.com>
2020-06-27 23:44:48 -0400

    added rescheduling postponed method

--- src/components/PostAppointment.vue
+++ src/components/PostAppointment.vue
@@ -13,30 +13,64 @@ export default {
     };
   },
   methods: {
-    ...mapActions(["addPostponed", "updatePostponed", "deletePostponed"]),
+    ...mapActions([
+      "addPostponed",
+      "updatePostponed",
+      "deletePostponed",
+      "addScheduledAppointment"
+    ]),
     addPostponedApp() {
       this.addPostponed({
         name: this.name,
         description: this.description
       });
+      this.name = "";
+      this.description = "";
     },
     updatePostponedApp() {
       this.updatePostponed({
         name: this.name,
         description: this.description
       });
+
+      this.name = "";
+      this.description = "";
     },
     deletePostponedApp() {
       this.deletePostponed({
         name: this.name,
         description: this.description
       });
+
+      this.name = "";
+      this.description = "";
+    },
+    schedulePostponed(scheduleData) {
+      this.deletePostponed({
+        name: this.name,
+        description: this.description
+      });
+
+      this.addScheduledAppointment({
+        id: scheduleData.id,
+        name: this.name,
+        description: this.description,
+        date: scheduleData.date,
+        startHour: scheduleData.startHour,
+        endHour: scheduleData.endHour
+      });
+
+      this.name = "";
+      this.description = "";
     }
   },
   computed: {
-    ...mapGetters(["getPostponed"]),
+    ...mapGetters(["getPostponed", "getScheduledAppointments"]),
     postponedAppointments() {
       return this.getPostponed;
+    },
+    scheduledAppintments() {
+      return this.getScheduledAppointments;
     }
   }
 };
--- tests/unit/postAppointment.spec.js
+++ tests/unit/postAppointment.spec.js
@@ -65,14 +65,15 @@ describe("Postponed Appointments", () => {
     const wrapper = mount(PostAppointment, { store, localVue, vuetify });
     wrapper.setData({ name: "Future Appointment", description: "Fuuuuture" });
     wrapper.vm.addPostponedApp();
-    let currentLengthP = wrapper.vm.$store.state.postponedAppointments.length;
-    let currentLengthA = wrapper.vm.$store.state.scheduledAppointments.length;
+    let currentLengthP = wrapper.vm.$store.state.postponedAppointments.length-1;
+    let currentLengthA = wrapper.vm.$store.state.scheduledAppointments.length+1;
     const appointmentRenewed = {
       id: "APP-2",
       date: "2020-06-26",
       startHour: "11:00",
       endHour: "12:00"
     };
+    wrapper.setData({ name: "Future Appointment", description: "Fuuuuture" });
     wrapper.vm.schedulePostponed(appointmentRenewed);
     assert.equal(
       currentLengthP,
